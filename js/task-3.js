ffcf50ed1285a594636e4b538df691ffdcfe1ede
Oleksii Zinchenko <17avzinchenko@gmail.com>
2025-06-26 15:05:30 +0200

    js-hw-08

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const textInput = document.getElementById("name-input");
-const nameOutput = document.getElementById("name-output");
-
-textInput.addEventListener("input", (event) => {
-  const trimValue = event.currentTarget.value.trim();
-  nameOutput.textContent = trimValue === "" ? "Anonymous" : trimValue;
-});
